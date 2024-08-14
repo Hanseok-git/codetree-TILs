9960d583a278dc311716b1785181dc0bd4f745f6
Hanseok-git <seogh31@gmail.com>
2024-08-14 16:22:46 +0900

    Title: 숫자의 계절은, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240814/숫자의 계절은/season-of-num.java
+++ 240814/숫자의 계절은/season-of-num.java
@@ -15,7 +15,7 @@ public class Main {
         if (9<=m && m<=11) {
             System.out.print("Fall");
         }
-        if (12==m && 1==m || m==2) {
+        if ((1<=m && m<=2) || m==12) {
             System.out.print("Winter");
         }
     }
