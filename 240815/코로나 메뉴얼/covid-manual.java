71e37569b4df1732c1c5787c6c628bc505624077
Hanseok-git <seogh31@gmail.com>
2024-08-15 20:50:29 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240815/코로나 메뉴얼/covid-manual.java
+++ 240815/코로나 메뉴얼/covid-manual.java
@@ -16,9 +16,9 @@ public class Main {
 
         if(c1 == 'Y' && t1 >= 37) {
             if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
-                System.out.println("E")
+                System.out.println("E");
             else
-                System.out.println("N")
+                System.out.println("N");
         }
         else {
             if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
