80e5c99d154359886546c961f6c6f2cb62ea5537
Hanseok-git <seogh31@gmail.com>
2024-08-16 00:40:22 +0900

    Title: n까지 숫자 출력, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240816/n까지 숫자 출력/print-1-to-n.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i = 1;
+
+        while (i <=n) {
+            if (i<n) {
+                System.out.print(i+" ");
+            }
+            else {
+                System.out.print(i);
+            }
+            i++;
+        }
+    }
+}
